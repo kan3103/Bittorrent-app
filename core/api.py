5a1e6b598cdd2a24fc890bdb66bbbf50ede1cc34
blueonline07 <ldkhang1201@gmail.com>
2024-12-03 14:43:53 +0700

    ok

--- core/api.py
+++ core/api.py
@@ -19,39 +19,38 @@ class Client:
         self.server = Server(self.torrents, PEER_PORT, self.strategy)
         self.server_thread = None
 
-    def create_torrent_from_file_paths(self, dir, file_paths):
+    def make_dir(dir, file_paths):
         os.makedirs(dir, exist_ok=True)
 
         for file_path in file_paths:
             shutil.copy(file_path, dir)
+        return dir
+
+    def create_torrent(self, dir):
         torrent = Torrent.create_torrent_file(dir)
         self.torrents[torrent.info_hash] = torrent
-        # print(os.path.abspath(self.torrent.name + ".torrent"))
+        return torrent
     
-    def create_torrent_from_dir(self, dir):
-        torrent = Torrent.create_torrent_file(dir)
+    def add_torrent(self, torrent_file):
+        torrent = Torrent(torrent_file)
         self.torrents[torrent.info_hash] = torrent
-        return torrent.info_hash
+        return torrent
+    
         
     def download(self, info_hash):
         torrent = self.torrents[info_hash]
-        # peers = requests.get(torrent.announce, params={'info_hash':torrent.info_hash, 'peer_id':self.server.peer_id, 'port':self.server.port, 'event':'started'}).json()
-        peers = [
-            # {'ip':'172.18.0.2', 'port':8001},
-            # {'ip':'172.18.0.4', 'port':8002},
-            # {'ip':'172.18.0.5', 'port':8003}
-            # {'peer_id':"DlcCX7j*$6!A,]%WF?qu", 'ip':'127.0.0.1', 'port':8001},
-            {'peer_id':'DlcCX7j*$6!A,]%WF?qu', 'ip':'127.0.0.1', 'port':8000},
-        ]
+        peers = requests.get(torrent.announce, params={'info_hash':torrent.info_hash, 'peer_id':self.server.peer_id, 'port':self.server.port, 'event':'started'}).json()
+        # peers = [
+        #     {'peer_id' :'DlcCX7j*$6!A,]%WF?qu', 'ip':'127.0.0.1', 'port':8000},
+        # ]
+        # print(peers)
 
         self.downloaders[info_hash] =  Downloader(torrent, peers, TitOrTat())
         print("Starting download")
         self.download_threads.append(Thread(target=self.downloaders[info_hash].start).start())
 
-    def add_torrent(self, torrent_file):
-        torrent = Torrent(torrent_file)
-        self.torrents[torrent.info_hash] = torrent
-        return torrent
+    def get_downloading_torrents(self):
+        return [val.torrent for val in self.downloaders.values()]
     
     def run_server(self):
         self.server_thread = Thread(target=self.server.start).start()
@@ -70,9 +69,10 @@ class Client:
 
 if __name__ == '__main__':
     client = Client()
-    info_hash = client.create_torrent_from_dir('downloads')
+    info_hash = client.create_torrent('downloads')
     client.run_server()
     time.sleep(1)
-    client.download(client.add_torrent('downloads.torrent'))
+    client.download(client.add_torrent('downloads.torrent').info_hash)
+    client.get_downloading_torrents()
     time.sleep(0.5)
     
--- core/constants.py
+++ core/constants.py
@@ -5,8 +5,9 @@ import socket
 PROTOCOL_NAME = 'BitTorrent protocol'
 PEER_ID = 'peer5678901234567890'
 PEER_IP = socket.gethostbyname(socket.gethostname()) or 'localhost'
-PEER_PORT = 8000
-TRACKER_URL = 'localhost:8000'
+PEER_PORT = 8001
+TRACKER_URL = 'http://127.0.0.1:8000/announce'
+# TRACKER_URL = 'http://172.16.0.165/8001/announce'
 # PIECE_SIZE = 524288 # 512 KB
 PIECE_SIZE =  100
 BLOCK_SIZE = 50
